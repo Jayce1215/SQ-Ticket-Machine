f9b3a74942deb505c186bf602b03b2a15057313d
Jayce <yoojaejim1215@gmail.com>
2024-08-20 15:08:51 -0700

    Done

--- app.py
+++ app.py
@@ -176,21 +176,27 @@ def schedule_step3():
     warrantyStatus = request.form['warrantyStatus']
 
         # Check if a record with the same key fields already exists
-    existing_record = UserInfo.query.filter_by(
+    existing_user = UserInfo.query.filter_by(
             name=name,
             contactNum=contactNum,
             serialNum=serialNum,
         ).first()
-    print(name, contactNum, serialNum)
-    print(existing_record)
+    logging.debug(name, contactNum, serialNum)
+    logging.debug(existing_user)
 
-    if existing_record:
-            unique_id = existing_record.unique_id
+    if existing_user:
+            unique_id = existing_user.unique_id
     else:
             unique_id = str(uuid.uuid4())
 
+    today = datetime.today().date()
+
     # Query the available dates where slot > 0
-    available_dates = ChatSlot.query.filter(ChatSlot.slot > 0).all()
+    available_dates = ChatSlot.query.filter(
+        ChatSlot.slot > 0, 
+        ChatSlot.zone == "E",
+        func.date(ChatSlot.from_dtime) >= today
+        ).all()
     
     # Extract the dates
     available_dates = [slot.from_dtime.strftime('%Y-%m-%d') for slot in available_dates]
@@ -244,83 +250,95 @@ def schedule_step3():
                            zipcode=zipcode, email=email,unique_id=unique_id, available_dates=available_dates)
 
 
-@app.route('/schedule/confirm', methods=['POST'])
-def schedule_confirm():
-    error_message = None
-    user_info = UserInfo.query.filter_by(unique_id=request.form['unique_id']).first()
-    try:
-        # Collect form data
-        selected_date = datetime.strptime(request.form['datepicker'] + " 00:00:00", '%Y-%m-%d %H:%M:%S')
-        name = request.form['name']
-        contactNum = request.form['contactNum']
-        address = request.form['address']
-        modelNum = request.form['modelNum']
-        product_type = product_data.get(modelNum, ("Unknown", "Unknown"))[0]
-        product_category = product_data.get(modelNum, ("Unknown", "Unknown"))[1]
-        serialNum = request.form['serialNum']
-        issue = request.form['issue']
-        city = request.form['city']
-        state = request.form['state']
-        zipcode = request.form['zipcode']
-        email = request.form['email']
-        warrantyStatus = request.form['warrantyStatus']
-        img_bos_path = request.form['img_bos_path']
-        img_issues_paths = request.form.getlist('img_issues_paths')
-        unique_id = request.form['unique_id']
-
-        chat_slot = ChatSlot.query.filter(
-            ChatSlot.zone == 'E',
-            func.date(ChatSlot.from_dtime) <= selected_date.date(),
-            func.date(ChatSlot.to_dtime) >= selected_date.date(),
-        ).first()
-
-        if chat_slot is None:
-            # No matching slot found
-            error_message = "No available slot found for the selected date. Please go back and choose another date."
-        elif chat_slot.slot <= 0:
-            # Slot is no longer available
-            error_message = "The selected slot is no longer available. Please go back and choose another date."
-        else:
-            # Proceed with the confirmation process if the slot is available
-            chat_slot.slot -= 1
-            chat_slot.updated_on = datetime.now()
+@app.route('/schedule/confirm/<unique_id>', methods=['POST'])
+def schedule_confirm(unique_id):
+    error_message_availability = None
+    error_message_user = None
+    user_info = UserInfo.query.filter_by(unique_id=unique_id).first()
+    print(unique_id)
+    
+    # Check if a record already exists
+    if user_info is not None:
+            today = datetime.today().date()
+            available_dates = ChatSlot.query.filter(
+            ChatSlot.slot > 0, 
+            ChatSlot.zone == "E",
+            func.date(ChatSlot.from_dtime) >= today
+            ).all()
+            available_dates = [slot.from_dtime.strftime('%Y-%m-%d') for slot in available_dates]
+            error_message_user = f"Your ticket({unique_id}) already exists. Please reschedule using the ticket number."
+            return render_template('schedule/step3.html', name=user_info.name, contactNum=user_info.contactNum, address=user_info.address, modelNum=user_info.modelNum, 
+                            serialNum=user_info.serialNum, img_bos_path=user_info.img_bos, img_issues_paths=user_info.img_issues.split(","), warrantyStatus=user_info.warrantyStatus, 
+                            city=user_info.city, state=user_info.state, zipcode=user_info.zipcode, email=user_info.email, issue=user_info.issue, selected_date=user_info.appointment_date, 
+                            product_type=user_info.product_type, product_category=user_info.product_category, unique_id=unique_id, error_message_user=error_message_user, available_dates=available_dates)
+    else:
+        try:
+            # Collect form data
+            selected_date = datetime.strptime(request.form['datepicker'] + " 00:00:00", '%Y-%m-%d %H:%M:%S')
+            name = request.form['name']
+            contactNum = request.form['contactNum']
+            address = request.form['address']
+            modelNum = request.form['modelNum']
+            product_type = product_data.get(modelNum, ("Unknown", "Unknown"))[0]
+            product_category = product_data.get(modelNum, ("Unknown", "Unknown"))[1]
+            serialNum = request.form['serialNum']
+            issue = request.form['issue']
+            city = request.form['city']
+            state = request.form['state']
+            zipcode = request.form['zipcode']
+            email = request.form['email']
+            warrantyStatus = request.form['warrantyStatus']
+            img_bos_path = request.form['img_bos_path']
+            img_issues_paths = request.form.getlist('img_issues_paths')
+            unique_id = request.form['unique_id']
+            
         
-        user = UserInfo.query.filter_by(unique_id=unique_id).first()
+            chat_slot = ChatSlot.query.filter(
+                    ChatSlot.zone == 'E',
+                    func.date(ChatSlot.from_dtime) <= selected_date.date(),
+                    func.date(ChatSlot.to_dtime) >= selected_date.date(),
+                ).first()
 
-        if not user == None:
-            user.appointment_date = selected_date
-        else:     
-            # Save the data to the database
-            user_info = UserInfo(
-                appointment_date=selected_date,
-                unique_id=unique_id,
-                name=name,
-                contactNum=contactNum,
-                address=address,
-                modelNum=modelNum,
-                product_type=product_type,
-                product_category=product_category,
-                serialNum=serialNum,
-                issue=issue,
-                city=city,
-                state=state,
-                zipcode=zipcode,
-                email=email,
-                warrantyStatus=warrantyStatus,
-                img_bos=img_bos_path,
-                img_issues=",".join(img_issues_paths),
-            )
+                # Check if the slot is available
+            if chat_slot is None or chat_slot.slot <= 0:
+                    error_message_availability = "The selected slot is no longer available. Please go back and choose another date."
+                    return render_template('schedule/step3.html', name=name ,unique_id=unique_id , error_message_availability=error_message_availability)
+            else:
+                    # Proceed with the confirmation process if the slot is available
+                    chat_slot.slot -= 1
+                    chat_slot.updated_on = datetime.now()
 
-        db.session.add(user_info)
-        db.session.commit()
-        
-        return render_template('schedule/confirm.html', name=name, contactNum=contactNum, address=address, modelNum=modelNum, 
-                               serialNum=serialNum, img_bos_path=img_bos_path, img_issues_paths=img_issues_paths, warrantyStatus=warrantyStatus, 
-                               city=city, state=state, zipcode=zipcode, email=email, issue=issue, selected_date=selected_date, 
-                               product_type=product_type, product_category=product_category, unique_id=unique_id, error_message=error_message)
-    except Exception as e:
-        logging.error(f'Error during confirmation: {e}')
-        return f'An error occurred during the file upload process.{unique_id}'
+                    # Save the data to the database
+                    user_info = UserInfo(
+                        appointment_date=selected_date,
+                        unique_id=unique_id,
+                        name=name,
+                        contactNum=contactNum,
+                        address=address,
+                        modelNum=modelNum,
+                        product_type=product_type,
+                        product_category=product_category,
+                        serialNum=serialNum,
+                        issue=issue,
+                        city=city,
+                        state=state,
+                        zipcode=zipcode,
+                        email=email,
+                        warrantyStatus=warrantyStatus,
+                        img_bos=img_bos_path,
+                        img_issues=",".join(img_issues_paths),
+                    )
+
+                    db.session.add(user_info)
+                    db.session.commit()
+                
+                    return render_template('schedule/confirm.html', name=name, contactNum=contactNum, address=address, modelNum=modelNum, 
+                                    serialNum=serialNum, img_bos_path=img_bos_path, img_issues_paths=img_issues_paths, warrantyStatus=warrantyStatus, 
+                                    city=city, state=state, zipcode=zipcode, email=email, issue=issue, selected_date=selected_date, 
+                                    product_type=product_type, product_category=product_category, unique_id=unique_id)
+        except Exception as e:
+            logging.error(f'Error during confirmation: {e}')
+            return f'An error occurred during the file upload process.{unique_id}'
 
 @app.route('/uploads/<unique_id>/<path:filename>')
 def uploads(unique_id,filename):
@@ -332,12 +350,66 @@ def reschedule_step1():
 
 @app.route('/reschedule/step2', methods=['POST'])
 def reschedule_step2():
-    ##routing with userID with args = request.args
-    return render_template('reschedule/step2.html')
+    ticketNum = request.form['ticketNum']
+    name = request.form['name']
+    contactNum = request.form['contactNum']
 
-@app.route('/reschedule/confirm', methods=['POST'])
-def reschedule_confirm():
-    return render_template('reschedule/confirm.html')
+    user = UserInfo.query.filter_by(unique_id=ticketNum , name=name , contactNum = contactNum ).first()    
+    today = datetime.today().date()
+
+    # Query the available dates where slot > 0
+    available_dates = ChatSlot.query.filter(
+        ChatSlot.slot > 0, 
+        ChatSlot.zone == "E",
+        func.date(ChatSlot.from_dtime) >= today
+        ).all()
+
+    # Extract the dates
+    available_dates = [slot.from_dtime.strftime('%Y-%m-%d') for slot in available_dates]
+
+    if user == None :
+        return render_template('reschedule/step1.html', error_message="Schedule not found",ticketNum=ticketNum, name=name, contactNum=contactNum)
+    else:
+        user_appointment_date = user.appointment_date
+        unique_id = user.unique_id
+        return render_template('reschedule/step2.html', unique_id=unique_id ,ticketNum=ticketNum, name=name, contactNum=contactNum, appointment_date = user_appointment_date, available_dates=available_dates)
+
+@app.route('/reschedule/confirm/<unique_id>', methods=['POST'])
+def reschedule_confirm(unique_id):
+    selected_date = datetime.strptime(request.form['datepicker'] + " 00:00:00", '%Y-%m-%d %H:%M:%S')
+    name = request.form['name']
+
+    chat_slot = ChatSlot.query.filter(
+            ChatSlot.zone == 'E',
+            func.date(ChatSlot.from_dtime) <= selected_date.date(),
+            func.date(ChatSlot.to_dtime) >= selected_date.date(),
+        ).first()
+    
+    # Check if the slot is available
+    if chat_slot is None or chat_slot.slot <= 0:
+        error_message_availability = "No available slot found for the selected date. Please go back and choose another date."
+        return render_template('reschedule/step2.html', unique_id=unique_id, error_message_availability=error_message_availability , name=name)
+
+    else:
+        # update new slot with -1
+        chat_slot.slot -= 1
+        chat_slot.updated_on = datetime.now()
+        
+        # update the original slot with +1
+        original_date = UserInfo.query.filter_by(unique_id=unique_id).first().appointment_date
+        ChatSlot.query.filter(
+            ChatSlot.zone == 'E',
+            func.date(ChatSlot.from_dtime) <= original_date.date(),
+            func.date(ChatSlot.to_dtime) >= original_date.date(),
+        ).update({'slot': ChatSlot.slot + 1})
+
+        # update the user info with the new date
+        UserInfo.query.filter_by(unique_id=unique_id).update({'appointment_date': selected_date})
+        
+                                                              
+        db.session.commit()
+        
+    return render_template('reschedule/confirm.html', selected_date=selected_date, original_date=original_date, unique_id=unique_id , name=name)
 
 @app.route('/cancel/step1')
 def cancel_step1():
@@ -345,12 +417,36 @@ def cancel_step1():
 
 @app.route('/cancel/step2',methods=['POST'])
 def cancel_step2():
-    ##routing with userID with args = request.args
-    return render_template('cancel/step2.html')
+    ticketNum = request.form['ticketNum']
+    name = request.form['name']
+    contactNum = request.form['contactNum']
 
-@app.route('/cancel/confirm',methods=['POST'])
-def cancel_confirm():
-    return render_template('cancel/confirm.html')
+    user = UserInfo.query.filter_by(unique_id=ticketNum , name=name , contactNum = contactNum ).first()    
+
+    if user == None :
+        return render_template('cancel/step1.html', error_message="Ticket not found",ticketNum=ticketNum, name=name, contactNum=contactNum)
+    else:
+        user_appointment_date = user.appointment_date
+        unique_id = user.unique_id
+        return render_template('cancel/step2.html', unique_id=unique_id , appointment_date = user_appointment_date)
+
+@app.route('/cancel/confirm/<unique_id>',methods=['POST'])
+def cancel_confirm(unique_id):
+    name = request.form['name']
+    user = UserInfo.query.filter_by(unique_id=unique_id).first()
+    appointment_date = user.appointment_date
+
+    # update the original slot with +1
+    ChatSlot.query.filter(
+            ChatSlot.zone == 'E',
+            func.date(ChatSlot.from_dtime) <= appointment_date.date(),
+            func.date(ChatSlot.to_dtime) >= appointment_date.date(),
+        ).update({'slot': ChatSlot.slot + 1, 'updated_on': datetime.now()})
+
+    db.session.delete(user)
+    db.session.commit()
+
+    return render_template('cancel/confirm.html', unique_id=unique_id, name=name) 
 
 
 admin.add_view(ModelView(UserInfo, db.session))
@@ -362,6 +458,7 @@ if __name__ == '__main__':
     app.run(port=8000, debug=True)
 
 
+# 1. time issue 2. 
 
 # Reschedule / Step 2 -> Find the date in ticket number and update the date in Slot to minus 1, the old date in slot to plus 1. if Sloct == 0, then show error message + update time stamp 
 # Cancel / Step 2 -> Find the date in ticket number and update the date in Slot to plus 1. + update time stamp
